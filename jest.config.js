dacac8ea1138790f72aeece072b4e7327bb95f75
Enoque Brito <enoquebrito@gmail.com>
2020-12-20 19:14:03 -0300

    chore: update on dependencies' configs

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   coverageDirectory: 'coverage',
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/**/*-protocols.ts',
+    '!**/protocols/**'
+  ],
   coverageProvider: "v8",
   transform: {
     '.+\\.ts$': 'ts-jest'
