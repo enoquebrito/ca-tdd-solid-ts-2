6da68ced26b6295013a0d559deb37d50da7c94d4
Enoque Brito <enoquebrito@gmail.com>
2021-02-01 00:31:50 -0300

    fix: remoção de await duplicado em um dos testes

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -68,7 +68,7 @@ describe('SignUp Controller', () => {
         passwordConfirmation: 'any_password'
       }
     }
-    const httpResponse = await await sut.handle(httpRequest)
+    const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('email'))
   })
