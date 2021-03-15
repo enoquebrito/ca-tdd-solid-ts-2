e7855991440a404231590eb4d45ea155675d5a14
Enoque Brito <enoquebrito@gmail.com>
2021-03-15 01:24:58 -0300

    feat: adicionado o signUpFactory

---
+++ src/1. cross-cutting/factories/signup.factory.ts
@@ -0,0 +1,16 @@
+import { SignUpController } from '../../2. presentation/controllers/signup/signup.controller'
+import { EmailValidatorAdapter } from '../../6. utils/email-validator/email-validator-adapter'
+import { AddAccountAppBusiness } from '../../3. application/usecases/add-account/add-account-app-business'
+import { BcryptAdapter } from '../../7. infra/criptografia/bcrypt-adapter'
+import { AddAccountMongoDb } from '../../7. infra/database/mongodb/usecases/add-account-mongodb'
+
+export const makeSignUpController = (): SignUpController => {
+  const salt = 12
+  const bcrypAdapter = new BcryptAdapter(salt)
+  const addAccountMongoDb = new AddAccountMongoDb()
+  const emailValidatorAdapter = new EmailValidatorAdapter()
+  const addAccountAppBusiness = new AddAccountAppBusiness(bcrypAdapter, addAccountMongoDb)
+  const signUpController = new SignUpController(emailValidatorAdapter, addAccountAppBusiness)
+
+  return signUpController
+}
