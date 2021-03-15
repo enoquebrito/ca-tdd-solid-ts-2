82aadf314ce2297eeb860f77f9d629876177700e
Enoque Brito <enoquebrito@gmail.com>
2021-03-15 01:04:15 -0300

    refactor: conectando ao mongodb antes dos testes de integracao

--- src/1. cross-cutting/routes/signup-routes.test.ts
+++ src/1. cross-cutting/routes/signup-routes.test.ts
@@ -1,7 +1,21 @@
+import { MongoDbHelper } from '../../7. infra/database/mongodb/helper/mongodb.helper'
 import request from 'supertest'
 import app from '../config/app'
 
 describe('SignUp Routes', () => {
+  beforeAll(async () => {
+    await MongoDbHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoDbHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    const accountCollection = MongoDbHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
   it('Should return an account on success', async () => {
     await request(app)
       .post('/api/signup')
